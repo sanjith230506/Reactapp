f89e9fe0e2e96442bb100c636f407de20683d888
sanjith230506 <sanjith.s2024aiml@sece.ac.in>
2025-12-19 10:16:11 +0530

    Update app.js

--- React App/backend/app.js
+++ React App/backend/app.js
@@ -1,30 +1,25 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+
 const productRoutes = require("./routes/productRoutes");
 const cartRoutes = require("./routes/cartRoutes");
-
-const connectDB = require("./db");
 const authRoutes = require("./routes/authRoutes");
+const connectDB = require("./db");
 
 const app = express();
 
-// middleware
 app.use(cors(
     {
-        origin:"https://13.60.37.52:7000",
-        credentials:true,
-
+        origin:"http://13.60.37.52:7000"
     }
 ));
 app.use(express.json());
-app.use("/api/products", productRoutes);
-app.use("/api/cart", cartRoutes);
 
-// DB
 connectDB();
 
-// routes
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 app.use("/api/auth", authRoutes);
 
 module.exports = app;
